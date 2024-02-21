7f73456806062ddd73ef4687aa34a710c855dafc
Henrique Pettenuci <henrique@pettenuci.com>
2024-02-21 00:29:41 -0300

    feat: add first version (#1)

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM golang:1.19 AS build-stage
+
+WORKDIR /app
+
+COPY go.mod go.sum ./
+RUN go mod download
+
+COPY *.go ./
+
+RUN CGO_ENABLED=0 GOOS=linux go build -o /prober
+
+# Deploy the application binary into a lean image
+FROM gcr.io/distroless/base-debian11 AS build-release-stage
+
+WORKDIR /
+
+COPY --from=build-stage /prober /prober
+
+EXPOSE 8080
+
+USER nonroot:nonroot
+
+ENTRYPOINT ["/prober"]
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+type configs struct {
+	Startup   string `json:"startup"`
+	Readiness string `json:"readiness"`
+	Liveness  string `json:"liveness"`
+}
+
+func getProbeDelay(probeEnv string) time.Duration {
+	probeDelay, e := os.LookupEnv(probeEnv)
+	if !e {
+		return 0
+	} else {
+		probeDelay, _ := strconv.ParseInt(probeDelay, 10, 64)
+		return time.Duration(probeDelay)
+	}
+}
+
+func startupProbe(c *gin.Context) {
+	time.Sleep(getProbeDelay("STARTUP_PROBE_DELAY") * time.Second)
+	c.JSON(http.StatusOK, gin.H{"message": "startup"})
+}
+
+func readinessProbe(c *gin.Context) {
+	time.Sleep(getProbeDelay("READINESS_PROBE_DELAY") * time.Second)
+	c.JSON(http.StatusOK, gin.H{"message": "readiness"})
+}
+
+func livenessProbe(c *gin.Context) {
+	time.Sleep(getProbeDelay("LIVENESS_PROBE_DELAY") * time.Second)
+	c.JSON(http.StatusOK, gin.H{"message": "liveness"})
+}
+
+func postConfigs(c *gin.Context) {
+	var newConfigs configs
+
+	if err := c.BindJSON(&newConfigs); err != nil {
+		return
+	}
+
+	os.Setenv("STARTUP_PROBE_DELAY", newConfigs.Startup)
+	os.Setenv("READINESS_PROBE_DELAY", newConfigs.Readiness)
+	os.Setenv("LIVENESS_PROBE_DELAY", newConfigs.Liveness)
+
+	c.IndentedJSON(http.StatusCreated, newConfigs)
+}
+
+func main() {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+
+	// Probes
+	router.GET("/startup", startupProbe)
+	router.GET("/readiness", readinessProbe)
+	router.GET("/liveness", livenessProbe)
+	// Config
+	router.POST("/config", postConfigs)
+
+	router.Run(":8080")
+}
+
